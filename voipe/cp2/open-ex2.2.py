603db66a02e0517330a86c075f8fdffb95d8db44
Ariful Islam <ariful6@yahoo.com>
2024-11-06 16:02:34 +0200

    open-ex1.3

--- voipe/cp2/open-ex2.2.py
+++ voipe/cp2/open-ex2.2.py
@@ -1,2 +1,11 @@
-x = 1, 2, 3, 4, 5, 6, 7, 8, 9
-y = -0.57, -2.57, -4.80, -7.36, -8.78, -10.52, -12.85, -14.69, -16.78
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+y = np.array([-0.57, -2.57, -4.80, -7.36, -8.78, -10.52, -12.85, -14.69, -16.78])
+
+plt.scatter(x, y, marker="+")
+plt.title("Scatter Plot of Points (x, y)")
+plt.xlabel("x values")
+plt.ylabel("y values")
+plt.show()
