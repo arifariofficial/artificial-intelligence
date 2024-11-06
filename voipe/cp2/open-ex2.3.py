8dc344a418d77fffff2fec737d747c08c0900370
Ariful Islam <ariful6@yahoo.com>
2024-11-06 19:42:21 +0200

    ex-2.4

--- voipe/cp2/open-ex2.3.py
+++ voipe/cp2/open-ex2.3.py
@@ -1,3 +1,13 @@
+""" 
+Read from CSV-file weight-height.csv to numpy-table information about the lengths and weights (in inches and pounds) of a group of students. Collect the lengths for the variable "length" and the weights for the variable "weight" by cutting the table.
+
+Convert lengths from inches to centimeters and weights from pounds to kilograms.
+
+Finally, calculate the means, medians, standard deviations, and variances of the lengths and weights.
+
+Extra: Draw a histogram of the lengths
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
---
+++ voipe/cp2/open-ex2.4.py
@@ -0,0 +1,29 @@
+""" 
+Look for a command from the numpy documentation (or elsewhere on the Internet) to calculate the matrix determinant. The determinant is a number that does not require a deeper understanding here. Calculate the determinants of the matrices A, B and AB, where
+
+A=\begin{pmatrix}
+1&2\\
+3&4
+\end{pmatrix}{,} B=\begin{pmatrix}
+-1  & 1\\
+5 & 7
+\end{pmatrix}
+
+and AB stands for matrix product. State that det (AB) = det (A) det (B) can be understood as the product of the numbers (the rounding error can be ignored).
+"""
+
+import numpy as np
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[-1, 1], [5, 7]])
+
+det_A = np.linalg.det(A)
+det_B = np.linalg.det(B)
+
+AB = np.dot(A, B)
+
+det_AB = np.linalg.det(AB)
+print(f"det(AB) = {det_AB}")
+
+product_of_dets = det_A * det_B
+print(f"det(A) * det(B) = {product_of_dets}")
